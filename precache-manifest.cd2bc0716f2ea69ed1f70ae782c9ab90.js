5152b58fc68b87e743728794f396c83f91d0a381
Erik11Lamela <65742900+Erik11Lamela@users.noreply.github.com>
2020-10-16 16:10:10 +0500

    Updates

---
+++ precache-manifest.cd2bc0716f2ea69ed1f70ae782c9ab90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ae8c4ed0c9aa028bdae37ecac30257",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "1fdd29a7ec073411f5b6",
+    "url": "/currency-converter/static/css/main.9c56b6d3.chunk.css"
+  },
+  {
+    "revision": "82dcf2b234778fb84a2b",
+    "url": "/currency-converter/static/js/2.04480cee.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/currency-converter/static/js/2.04480cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdd29a7ec073411f5b6",
+    "url": "/currency-converter/static/js/main.50452e83.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
